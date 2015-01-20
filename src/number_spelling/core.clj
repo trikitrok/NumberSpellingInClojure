c7ce05984353af6167de6750db136cea04aebac5
trikitrok <trikitrok@gmail.com>
2015-01-20 04:38:11 +0100

    Renamed some functions

--- src/number_spelling/core.clj
+++ src/number_spelling/core.clj
@@ -4,37 +4,37 @@
   one-word-numbers
   num-digits
   pow
-  spell-number-over-99
-  spell-number-up-to-99)
+  spell-out-number-over-99
+  spell-out-number-up-to-99)
 
-(defn spell [number]
+(defn spell-out [number]
   (cond 
-    (> (num-digits number) 9) (spell-number-over-99 number 9 " billion" ", ")
+    (> (num-digits number) 9) (spell-out-number-over-99 number 9 " billion" ", ")
     
-    (> (num-digits number) 6) (spell-number-over-99 number 6 " million" ", ")
+    (> (num-digits number) 6) (spell-out-number-over-99 number 6 " million" ", ")
     
-    (> (num-digits number) 3) (spell-number-over-99 number 3 " thousand" ", ")
+    (> (num-digits number) 3) (spell-out-number-over-99 number 3 " thousand" ", ")
     
-    (> (num-digits number) 2) (spell-number-over-99 number 2 " hundred" " and ")
+    (> (num-digits number) 2) (spell-out-number-over-99 number 2 " hundred" " and ")
     
-    :else (spell-number-up-to-99 number)))
+    :else (spell-out-number-up-to-99 number)))
 
-(defn- spell-number-over-99 [number digits separator1 separator2]
+(defn- spell-out-number-over-99 [number digits separator1 separator2]
   (let [power-of-ten (pow 10 digits)
         over-power-of-ten (rem number power-of-ten)]
-    (str (spell (quot number power-of-ten))
+    (str (spell-out (quot number power-of-ten))
          separator1
          (if (zero? over-power-of-ten) 
            ""
-           (str separator2 (spell over-power-of-ten))))))
+           (str separator2 (spell-out over-power-of-ten))))))
 
-(defn- spell-number-up-to-99 [number]
+(defn- spell-out-number-up-to-99 [number]
   (if-let [one-word-number (get one-word-numbers number)]
     one-word-number 
-    (str (spell (* (quot number 10) 10))
+    (str (spell-out (* (quot number 10) 10))
          (if (zero? (rem number 10))
            ""
-           (str " " (spell (rem number 10)))))))
+           (str " " (spell-out (rem number 10)))))))
 
 (defn- pow [base exp]
   (reduce * (repeat exp base)))
--- test/number_spelling/core_test.clj
+++ test/number_spelling/core_test.clj
@@ -7,61 +7,61 @@
   
   (fact 
     "it can spell one-digit numbers" 
-    (spell 0) => "zero"
-    (spell 1) => "one"
-    (spell 9) => "nine")
+    (spell-out 0) => "zero"
+    (spell-out 1) => "one"
+    (spell-out 9) => "nine")
   
   (fact 
     "it can spell two-digit numbers" 
-    (spell 10) => "ten"
-    (spell 11) => "eleven"
-    (spell 19) => "nineteen"
-    (spell 20) => "twenty"
-    (spell 21) => "twenty one"
-    (spell 30) => "thirty"
-    (spell 39) => "thirty nine"
-    (spell 95) => "ninety five")
+    (spell-out 10) => "ten"
+    (spell-out 11) => "eleven"
+    (spell-out 19) => "nineteen"
+    (spell-out 20) => "twenty"
+    (spell-out 21) => "twenty one"
+    (spell-out 30) => "thirty"
+    (spell-out 39) => "thirty nine"
+    (spell-out 95) => "ninety five")
   
   (fact 
     "it can spell three-digit numbers" 
-    (spell 100) => "one hundred"
-    (spell 103) => "one hundred and three"
-    (spell 199) => "one hundred and ninety nine"
-    (spell 999) => "nine hundred and ninety nine")
+    (spell-out 100) => "one hundred"
+    (spell-out 103) => "one hundred and three"
+    (spell-out 199) => "one hundred and ninety nine"
+    (spell-out 999) => "nine hundred and ninety nine")
   
   (fact
     "it can spell four-digit numbers"
-    (spell 1000) => "one thousand"
-    (spell 1501) => "one thousand, five hundred and one"
-    (spell 9000) => "nine thousand"
-    (spell 9999) => "nine thousand, nine hundred and ninety nine")
+    (spell-out 1000) => "one thousand"
+    (spell-out 1501) => "one thousand, five hundred and one"
+    (spell-out 9000) => "nine thousand"
+    (spell-out 9999) => "nine thousand, nine hundred and ninety nine")
   
   (fact
     "it can spell five-digit numbers"
-    (spell 21501) => "twenty one thousand, five hundred and one"
-    (spell 99999) => "ninety nine thousand, nine hundred and ninety nine")
+    (spell-out 21501) => "twenty one thousand, five hundred and one"
+    (spell-out 99999) => "ninety nine thousand, nine hundred and ninety nine")
   
   (fact
     "it can spell six-digit numbers"
-    (spell 999999) => "nine hundred and ninety nine thousand, nine hundred and ninety nine")
+    (spell-out 999999) => "nine hundred and ninety nine thousand, nine hundred and ninety nine")
   
   (fact
     "it can spell seven-digit numbers"
-    (spell 9999999) => "nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
+    (spell-out 9999999) => "nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
   
   (fact
     "it can spell eight-digit numbers"
-    (spell 99999999) => "ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
+    (spell-out 99999999) => "ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
   
   (fact
     "it can spell nine-digit numbers"
-    (spell 999999999) => "nine hundred and ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
+    (spell-out 999999999) => "nine hundred and ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
   
   (fact
     "it can spell ten-digit numbers"
-    (spell 9999999999) => "nine billion, nine hundred and ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
+    (spell-out 9999999999) => "nine billion, nine hundred and ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
   
   (fact
     "it can spell a lot of billions"
-    (spell 9999999999999) => 
+    (spell-out 9999999999999) => 
     "nine thousand, nine hundred and ninety nine billion, nine hundred and ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine"))
