3a1e87919c78b3da27c3449b3051ba6ce4647ca1
trikitrok <trikitrok@gmail.com>
2015-01-20 04:10:55 +0100

    Simplifying function to spell numbers under 100

--- src/number_spelling/core.clj
+++ src/number_spelling/core.clj
@@ -7,50 +7,50 @@
   separators
   closest-power-of-ten
   postfixes
-  spell-number-up-to-thousand
+  spell-number-under-100
   spell-number)
 
 (defn spell [number]
   (cond 
-    (> (num-digits number) 9) (spell-number number 9 "billion")
+    (> (num-digits number) 9) (spell-number number 9 "billion" ", ")
     
-    (> (num-digits number) 6) (spell-number number 6 "million")
+    (> (num-digits number) 6) (spell-number number 6 "million" ", ")
     
-    (> (num-digits number) 3) (spell-number number 3 "thousand")
+    (> (num-digits number) 3) (spell-number number 3 "thousand" ", ")
+    
+    (> (num-digits number) 2) (spell-number number 2 "hundred" " and ")
         
-    :else (spell-number-up-to-thousand number)))
+    :else (spell-number-under-100 number)))
 
-(defn- spell-number [number digits separator]
+(defn- spell-number [number digits separator1 separator2]
   (let [power-of-ten (pow 10 digits)
         over-power-of-ten (rem number power-of-ten)]
     (str (spell (quot number power-of-ten))
          " " 
-         separator
+         separator1
          (if (zero? over-power-of-ten) 
            ""
-           (str ", " (spell over-power-of-ten))))))
+           (str separator2 (spell over-power-of-ten))))))
 
-(defn- spell-number-up-to-thousand [number]
+(defn- spell-number-under-100 [number]
   (if-let [one-word-number (get one-word-numbers number)]
     one-word-number
     
     (let [num-digits (num-digits number)
-          multiple-of-closest-power-of-ten (quot number (pow 10 (dec num-digits)))
+          multiple-of-closest-power-of-ten (quot number 10)
           closest-power-of-ten (closest-power-of-ten number num-digits)
           num-minus-closest-power-of-ten (- number closest-power-of-ten)]
       
       (str 
         (if-let [one-word-number (get one-word-numbers closest-power-of-ten)]
           one-word-number
-          (spell-number-up-to-thousand multiple-of-closest-power-of-ten))
-        
-        (get postfixes num-digits)
+          (spell-number-under-100 multiple-of-closest-power-of-ten))
         
         (if (zero? num-minus-closest-power-of-ten)
           ""
           (str
             (get separators num-digits) 
-            (spell-number-up-to-thousand num-minus-closest-power-of-ten)))))))
+            (spell-number-under-100 num-minus-closest-power-of-ten)))))))
 
 (defn- closest-power-of-ten [number num-digits]
   (* (quot number (pow 10 (dec num-digits))) 
