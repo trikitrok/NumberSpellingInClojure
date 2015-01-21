1824a511026379fbab8e0fe7010aa7c301bb4a0d
trikitrok <trikitrok@gmail.com>
2015-01-22 00:40:13 +0100

    Refactored to remove some duplication

--- src/number_spelling/core.clj
+++ src/number_spelling/core.clj
@@ -4,37 +4,36 @@
   one-word-numbers
   num-digits
   pow
-  spell-out-number-over-99
-  spell-out-number-up-to-99)
+  number-spelling)
 
 (defn spell-out [number]
-  (cond 
-    (> (num-digits number) 9) (spell-out-number-over-99 number 9 " billion" ", ")
-    
-    (> (num-digits number) 6) (spell-out-number-over-99 number 6 " million" ", ")
-    
-    (> (num-digits number) 3) (spell-out-number-over-99 number 3 " thousand" ", ")
-    
-    (> (num-digits number) 2) (spell-out-number-over-99 number 2 " hundred" " and ")
-    
-    :else (spell-out-number-up-to-99 number)))
-
-(defn- spell-out-number-over-99 [number digits separator1 separator2]
-  (let [power-of-ten (pow 10 digits)
-        over-power-of-ten (rem number power-of-ten)]
-    (str (spell-out (quot number power-of-ten))
-         separator1
-         (if (zero? over-power-of-ten) 
-           ""
-           (str separator2 (spell-out over-power-of-ten))))))
-
-(defn- spell-out-number-up-to-99 [number]
   (if-let [one-word-number (get one-word-numbers number)]
-    one-word-number 
-    (str (spell-out (* (quot number 10) 10))
-         (if (zero? (rem number 10))
-           ""
-           (str " " (spell-out (rem number 10)))))))
+    one-word-number
+    (let [num-digits (num-digits number)]
+      (cond 
+        (> num-digits 9) 
+          (number-spelling number 1 9 " billion" ", ")
+        
+        (> num-digits 6) 
+          (number-spelling number 1 6 " million" ", ")
+        
+        (> num-digits 3) 
+          (number-spelling number 1 3 " thousand" ", ")
+        
+        (> num-digits 2) 
+          (number-spelling number 1 2 " hundred" " and ")
+        
+        :else 
+          (number-spelling number 10 1 "" " ")))))
+
+(defn- number-spelling [number factor exp separator1 separator2]
+  (let [power-of-ten (pow 10 exp)
+        close-number (* factor (quot number power-of-ten))
+        rest-of-number (rem number power-of-ten)]
+    (str (spell-out close-number)
+         separator1
+         (when-not (zero? rest-of-number) 
+           (str separator2 (spell-out rest-of-number))))))
 
 (defn- pow [base exp]
   (reduce * (repeat exp base)))
